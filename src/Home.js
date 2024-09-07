001af0e9a45874d1c812d3c1eafc14498fdbc4b8
Anirudh Pai <anirudhnpai@gmail.com>
2024-09-07 15:18:49 -0700

    home page init

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="main">
+            <h1 className="main-header">Welcome to Anirudh Pai's CS180 Website</h1>
+        </div>
+    );
+}
+
+export default Home;
