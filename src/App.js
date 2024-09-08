34cc1f101762da9ca8b46ed9b1ed3850294011c1
Anirudh Pai <anirudhnpai@gmail.com>
2024-09-08 15:04:46 -0700

    back to browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 import Proj01 from "./components/projects/proj01/Proj01";
 
