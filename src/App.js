dea4a44485c91495fd28141dfd19f98e92beca54
Anirudh Pai <anirudhnpai@gmail.com>
2024-09-08 14:22:52 -0700

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 import Proj01 from "./components/projects/proj01/Proj01";
 
 // App Component
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/cs180" element={<Home />} />
         <Route path="/cs180/proj01" element={<Proj01 />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
