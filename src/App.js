838ec3d713745e5d6b65d74c1a4e955eba587615
Anirudh Pai <anirudhnpai@gmail.com>
2024-09-21 23:23:41 -0700

    proj02 init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 import Proj01 from "./components/projects/proj01/Proj01";
+import Proj02 from "./components/projects/proj02/Proj02";
 
 // App Component
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/proj01" element={<Proj01 />} />
+        <Route path="/proj02" element={<Proj02 />} />
       </Routes>
     </Router>
   );
---
+++ src/components/projects/proj02/Proj02.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Proj02.css';
+
+const Proj02 = () => {
+
+    return (
+        <div className="main">
+            <h1 className="main-header">Project #2: Fun With Filters and Frequencies</h1>
+            <div className="content">
+                <h1 className="lvl2-header">Overview</h1>
+                <p>
+                    
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Proj02;
