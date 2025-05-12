5af7712ed790cd8945ec0f8c45fef7245b695d7f
Anirudh Pai <anirudhnpai@gmail.com>
2025-05-11 21:24:29 -0700

    remove unecessary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
-import Proj01 from "./components/projects/proj01/Proj01";
-import Proj02 from "./components/projects/proj02/Proj02";
-import Proj03 from "./components/projects/proj03/Proj03";
-import Proj04 from "./components/projects/proj04/Proj04";
-import Proj05 from "./components/projects/proj05/Proj05";
-import NeRF from "./components/projects/nerf/NeRF";
 import CS280Final from "./components/projects/cs280-final/CS280Final";
 
 
@@ -15,12 +9,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/proj01" element={<Proj01 />} />
-        <Route path="/proj02" element={<Proj02 />} />
-        <Route path="/proj03" element={<Proj03 />} />
-        <Route path="/proj04" element={<Proj04 />} />
-        <Route path="/proj05" element={<Proj05 />} />
-        <Route path="/nerf" element={<NeRF />} />
         <Route path="/cs280-final" element={<CS280Final />} />
       </Routes>
     </Router>
